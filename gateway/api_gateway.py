d9e3f9708e812e2abcf9b214537012ca817afa5e
lennyhurni <148008224+lennyhurni@users.noreply.github.com>
2024-10-31 17:40:59 +0100

    Refactor API gateway routes: simplify user, product, cart, sales, and returns service endpoints

--- gateway/api_gateway.py
+++ gateway/api_gateway.py
@@ -5,35 +5,40 @@ app = Flask(__name__)
 
 # Route für User-Service
 @app.route('/users/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
-def users_proxy(path):
+@app.route('/users', methods=['GET', 'POST'])
+def users_proxy(path=''):
     url = f'http://user_management_service:5000/{path}'  # URL des User-Service Containers
     response = requests.request(method=request.method, url=url, json=request.get_json())
     return jsonify(response.json()), response.status_code
 
 # Route für Product-Service
 @app.route('/products/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
-def products_proxy(path):
+@app.route('/products', methods=['GET', 'POST'])
+def products_proxy(path=''):
     url = f'http://product_management_service:5002/{path}'  # URL des Product-Service Containers
     response = requests.request(method=request.method, url=url, json=request.get_json())
     return jsonify(response.json()), response.status_code
 
 # Route für Cart-Service
 @app.route('/carts/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
-def carts_proxy(path):
+@app.route('/carts', methods=['GET', 'POST'])
+def carts_proxy(path=''):
     url = f'http://cart_order_management_service:5003/{path}'  # URL des Cart-Service Containers
     response = requests.request(method=request.method, url=url, json=request.get_json())
     return jsonify(response.json()), response.status_code
 
 # Route für Sales-Service
 @app.route('/sales/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
-def sales_proxy(path):
+@app.route('/sales', methods=['GET', 'POST'])
+def sales_proxy(path=''):
     url = f'http://sales_payment_service:5001/{path}'  # URL des Sales-Service Containers
     response = requests.request(method=request.method, url=url, json=request.get_json())
     return jsonify(response.json()), response.status_code
 
 # Route für Return-Service
 @app.route('/returns/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
-def returns_proxy(path):
+@app.route('/returns', methods=['GET', 'POST'])
+def returns_proxy(path=''):
     url = f'http://return_refund_service:5004/{path}'  # URL des Return-Service Containers
     response = requests.request(method=request.method, url=url, json=request.get_json())
     return jsonify(response.json()), response.status_code
